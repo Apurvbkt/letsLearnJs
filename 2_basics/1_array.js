8c971a1ba6a32d571b0a7a256cfa777fe36b5c1c
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-04 16:29:00 +0000

    good night

--- 2_basics/1_array.js
+++ 2_basics/1_array.js
@@ -8,8 +8,24 @@ let myArr = [1,2,3,4,5,6,7]
 // myArr.pop()
 // myArr.unshift(9)
 // myArr.shift()
-let newArr =  myArr.join()
+// let newArr =  myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
 
 console.log(myArr);
-console.log( newArr);
+
+let myr1 = myArr.slice(1,3) //index of 1 se suru hoga 3 tak and print hoga index 1 and 2 and end index ko exclude krdega. And real arr same rahega
+console.log('A ' + myr1);
+console.log(myArr);
+
+
+
+let myr2 = myArr.splice(1,3) //index of 1 se leke 3 tak, sabko print krega, but real arr ko bhi change krdega
+console.log('B '+ myr2);
+console.log(myArr);
+
+
+
+
 
