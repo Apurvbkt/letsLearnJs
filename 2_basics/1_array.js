6504395f3734553b8e3d224e2dd7a20b4767e1dc
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-04 15:08:33 +0000

    updatea arr

--- 2_basics/1_array.js
+++ 2_basics/1_array.js
@@ -1,10 +1,13 @@
 let myArr = [1,2,3,4,5,6,7]
 // console.log(myArr[1]);
 
-myArr.push(8)
-myArr.push(9)
-myArr.push(10)
-myArr.pop()
-myArr.pop()
+// myArr.push(8)
+// myArr.push(9)
+// myArr.push(10)
+// myArr.pop()
+// myArr.pop()
+// myArr.unshift(9)
+myArr.shift()
+
 console.log(myArr);
 
