32d984e96cf22f7a5cad2e700e94fd2497e59f98
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-04 14:47:43 +0000

    practice data and time

---
+++ 1_basics/datePrac.js
@@ -0,0 +1,28 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+let newDate = new Date("2001-05-22")
+
+// console.log(newDate.toDateString());
+// console.log(newDate.toLocaleString());
+
+
+let phirSe = new Date()
+console.log(phirSe);
+
+// console.log(phirSe.getDate());
+// console.log(phirSe.getFullYear());
+// console.log(phirSe.getMonth());
+// console.log(phirSe.getTime());
+
+
+let fullDate = new Date()
+console.log(`today's date  ${fullDate.getDate()}/ ${fullDate.getMonth() + 1}/ ${fullDate.getFullYear()}`);
+
+
