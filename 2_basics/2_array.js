dab202199f319238b2a358ee6bcbe9fa6baead93
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-05 15:39:13 +0000

    good nighr

--- 2_basics/2_array.js
+++ 2_basics/2_array.js
@@ -15,7 +15,23 @@ let allNewHeros = [...marvel, ...dc]
 
 const longArr = [1,2,3,4,[5,6,7],8,9,10,[11,12,13,14,15,[16,17,18,19]]]
 let newArr = longArr.flat(Infinity)
-console.log(newArr);
+// console.log(newArr);
+
+ 
+ console.log(Array.isArray("Apurv"))
+ console.log(Array.from("Apurv"))
+
+ let score1 = 100
+ let score2 = 200
+ let score3 = 300
+ let combine = Array.of(score1,score2,score3)
+
+ console.log(combine[1]);
+ 
+
+ 
+
+
 
 
 
