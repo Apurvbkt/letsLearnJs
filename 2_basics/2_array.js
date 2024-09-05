c73ffd453d5cb53db82e601f51a231b99845de92
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-05 14:43:33 +0000

    all in one arr

--- 2_basics/2_array.js
+++ 2_basics/2_array.js
@@ -11,8 +11,11 @@ let dc = ["Batman", "Superman", "Flash"];
 // console.log(...marvel, ...dc);
 
 let allNewHeros = [...marvel, ...dc]
-console.log(allNewHeros);
+// console.log(allNewHeros);
 
+const longArr = [1,2,3,4,[5,6,7],8,9,10,[11,12,13,14,15,[16,17,18,19]]]
+let newArr = longArr.flat(Infinity)
+console.log(newArr);
 
 
 
