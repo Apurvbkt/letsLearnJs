efb294997ad33ad787c966bd6d171a9dd8e840a5
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-05 14:24:44 +0000

    all hero

---
+++ 2_basics/2_array.js
@@ -0,0 +1,12 @@
+let marval = ["ironMan", "spiderMan", "Thor"];
+let dc = ["Batman", "Superman", "Flash"];
+
+// marval.push(dc);
+// console.log(marval);
+// console.log(marval[3][2]);
+
+let allHeros = marval.concat(dc)
+console.log(allHeros);
+
+
+
