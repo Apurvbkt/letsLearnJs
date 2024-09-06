d193e6d999e03afa8362fd327ab6347ed1e03504
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-06 16:37:29 +0000

    learn

--- 2_basics/3_object.js
+++ 2_basics/3_object.js
@@ -1,8 +1,24 @@
 // object literals
+
+let mysym = Symbol("key1")
+
 let userJs = {
     name:"apurv",
+    [mysym] : "mykey1",
+    "full name" : "Apurv Bhakat",
     age: 22,
     location: 'jsr',
     email : 'apu@gmail.com',
     isloggedin : false
 }
+
+userJs.email = "gpt@gmail.com"
+
+console.log(userJs.name);
+console.log(userJs["name"]);
+// console.log(userJs[mysym]);
+console.log(userJs["full name"]);
+console.log(userJs.email);
+
+
+
