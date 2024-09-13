71639fc77bcf2eb13cc47bc31d68ea6d6ed3404f
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-13 15:18:01 +0000

    iife done

--- 3_basics/3_arrow.js
+++ 3_basics/3_arrow.js
@@ -34,7 +34,9 @@ let chai = ()=>{
 // }
 
 
+// const addTwo = (num1, num2)=> num1 + num2
 
-const addTwo = (num1, num2)=> num1 + num2
+const addTwo = (num1, num2)=> (num1 + num2)
+
 
 console.log(addTwo(1,2));
---
+++ 3_basics/4_iife.js
@@ -0,0 +1,14 @@
+(function chai(){
+    console.log('my chai');
+    
+})();
+
+(()=>{
+    console.log('meri chai');
+    
+})();
+
+((name)=>{
+    console.log(`my name is ${name} and I love Chai`);
+    
+})('apurv')
