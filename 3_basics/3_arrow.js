8651f9183cbe520cce78c0e9cb65d5ce1b4a113e
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-13 06:09:42 +0000

    arrow obj

---
+++ 3_basics/3_arrow.js
@@ -0,0 +1,10 @@
+const obj = {
+    name : 'Apurv',
+    price : 999,
+    detail : function(){
+       console.log(`name is ${this.name} and paid ${this.price}`);
+        
+        
+    }
+}
+ obj
