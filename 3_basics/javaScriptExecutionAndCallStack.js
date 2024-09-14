45b8650e8dc786bfb7d72671f0491c45edb22b30
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-14 16:42:12 +0000

    update

--- 3_basics/javaScriptExecutionAndCallStack.js
+++ 3_basics/javaScriptExecutionAndCallStack.js
@@ -8,4 +8,6 @@
 // 2 memory phase
 // 3 executaion phase
 
-// for function, new variable envirment + Execution context use to create to run function
+// for function, new variable envirment + Execution context use to create to run function. New sandbox created for function
+// and after this, again all the factors going to be repeat Line no. 7,8,9
+
