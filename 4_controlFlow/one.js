38443915ff3f9491dcc6b592a076553740543d95
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-15 17:47:13 +0000

    if else

--- 4_controlFlow/one.js
+++ 4_controlFlow/one.js
@@ -41,3 +41,7 @@ const userLoggedIn = true
 const debitCard = true
 const loggedInFromGoogle = false
 const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
