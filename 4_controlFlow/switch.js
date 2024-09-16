487fb5cc33b780234d8c6b6c565a9a110cab7130
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-16 13:23:45 +0000

    switch

---
+++ 4_controlFlow/switch.js
@@ -0,0 +1,21 @@
+const march = 'mafarch';
+
+switch (march) {
+  case 'jan':
+    console.log("jan");
+    break;
+  case 'feb':
+    console.log("Feb");
+    break;
+  case 'march':
+    console.log("mar");
+    break;
+  case 4:
+    console.log("apr");
+    break;
+
+  default:
+    console.log("nahi mila");
+
+    break;
+}
