19fe68886a6b9a00a2d6516a8f8a8e3c16b1d413
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-16 16:07:45 +0000

    inner loop outer loop

--- 5_iterations/one.js
+++ 5_iterations/one.js
@@ -2,7 +2,23 @@
 
 for (let index = 0; index < 10; index++) {
     const element = index;
-    console.log(element);
+    if (index == 5) {
+        // console.log('5 is the best number');
+        
+    }
+    // console.log(element);
+    
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`outer loop ${i}`);
+    for (let j = 0; j <=10; j++) {
+        
+        console.log(`inner loop ${j} and outer loop is ${i}`);
+        
+        
+    }
     
     
 }
