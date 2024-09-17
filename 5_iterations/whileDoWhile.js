8c03b15a3ac768a398df4e0c0a6cab8821aca119
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-17 03:21:40 +0000

    hogya while and do while

--- 5_iterations/whileDoWhile.js
+++ 5_iterations/whileDoWhile.js
@@ -10,6 +10,23 @@ let array = ["superman", "batman", "ironMan"];
 let arr = 0;
 
 while (arr < array.length) {
-  console.log(`super heros = ${array[arr]}`);
+//   console.log(`super heros = ${array[arr]}`);
   arr++;
 }
+
+let score = 1
+do {
+    // console.log(`score should be ${score}`);
+    score++
+    // console.log(score);
+    
+} while (score <= 10);
+
+
+let score2 = 11
+do {
+    console.log(`score2 should be ${score2}`);
+    score2++
+    console.log(score2);
+    
+} while (score2 <= 10);
