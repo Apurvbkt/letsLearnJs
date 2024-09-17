e73210b2afb22bbb6e0b4297885f9b5e0f9f4ba2
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-17 03:08:02 +0000

    while loop

---
+++ 5_iterations/whileDoWhile.js
@@ -0,0 +1,15 @@
+// while loop
+
+let count = 0;
+while (count <= 10) {
+  // console.log(`number is ${count}`);
+  count++;
+}
+
+let array = ["superman", "batman", "ironMan"];
+let arr = 0;
+
+while (arr < array.length) {
+  console.log(`super heros = ${array[arr]}`);
+  arr++;
+}
