0b5d19030e771113a8b6a7eeab29f8fd5c2b3504
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-17 15:28:47 +0000

    forEach

---
+++ 5_iterations/forEach.js
@@ -0,0 +1,28 @@
+const arry = ["one", "two", "three", "four", "five", "six", "seven"];
+
+arry.forEach((el) => {
+//   console.log(el);
+});
+
+const marvelHeroes = [
+  "Iron Man",
+  "Captain America",
+  "Thor",
+  "Hulk",
+  "Black Widow",
+  "Spider-Man",
+  "Doctor Strange",
+  "Black Panther",
+  "Scarlet Witch",
+  "Ant-Man",
+  "Hawkeye",
+  "Captain Marvel",
+  "Wolverine",
+  "Deadpool",
+  "Vision",
+];
+
+marvelHeroes.forEach((el)=>{
+    console.log(el);
+    
+})
