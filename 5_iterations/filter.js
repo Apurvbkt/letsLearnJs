ed3443d4dd2cbf0384efc7c0d1f2f7954fc15050
Apurva bhakat <91117738+Apurvbkt@users.noreply.github.com>
2024-09-18 14:28:55 +0000

    filter with if

--- 5_iterations/filter.js
+++ 5_iterations/filter.js
@@ -1,9 +1,29 @@
-const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+const lang = ['java', 'javascript', 'ruby', 'c++', 'c', 'python' ]
+
+// let resp=lang.forEach((el)=>console.log(el))
+
+
+
+
+
+// const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
 // let res = myNum.filter((num)=> num >5)
 // console.log(res);
 
-let res = myNum.filter((num)=> {
-   return num>5
-})
-console.log(res);
+// let res = myNum.filter((num)=> {
+//    return num>5
+// })
+// console.log(res);
+
+
+// const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+// const newNum = []
+
+// myNum.forEach((el)=>{
+//     if (el < 5) {
+//         newNum.push(el)
+//         console.log(el);
+//     }
+    
+// })
