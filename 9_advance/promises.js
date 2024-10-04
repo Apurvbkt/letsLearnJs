2ec4667d88107a5921a2c4948b599a0800e14387
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-04 21:31:06 +0530

    test

--- 9_advance/promises.js
+++ 9_advance/promises.js
@@ -15,27 +15,30 @@
 //baaki baateyn ab kal krnge
 
 
-new Promise(function(resolve, reject){
-    setTimeout(( )=>{
-        console.log('async task 2')
-        resolve()
-    },5000)
-}).then(function(){
-    console.log('2 task done');
+// new Promise(function(resolve, reject){
+//     setTimeout(( )=>{
+//         console.log('async task 2')
+//         resolve()
+//     },5000)
+// }).then(function(){
+//     console.log('2 task done');
     
-})
+// })
 
-const promiseThree= Promise((resolve, reject)=>{
+new Promise((resolve, reject)=>{
     setTimeout(()=>{
        
         resolve({name  : 'Apurv', gmail : 'apu@gmail.com'})
     },3000)
     
-})
-
-promiseThree.then(()=>{
-    console.log();
+}).then((user)=>{
+    console.log(user);
     
 })
+
+// promiseThree.then((user)=>{
+//     console.log(user);
+    
+// })
 
 
