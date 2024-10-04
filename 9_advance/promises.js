cb5134d5459f8cbf80df5c7736891bb5ab9724bb
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-04 21:29:05 +0530

    promiss three

--- 9_advance/promises.js
+++ 9_advance/promises.js
@@ -18,8 +18,24 @@
 new Promise(function(resolve, reject){
     setTimeout(( )=>{
         console.log('async task 2')
+        resolve()
     },5000)
 }).then(function(){
     console.log('2 task done');
     
 })
+
+const promiseThree= Promise((resolve, reject)=>{
+    setTimeout(()=>{
+       
+        resolve({name  : 'Apurv', gmail : 'apu@gmail.com'})
+    },3000)
+    
+})
+
+promiseThree.then(()=>{
+    console.log();
+    
+})
+
+
