f127b8bbc57370ecbbafcff5134e86a975387cb5
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-05 12:18:18 +0530

    try catch

--- 9_advance/promises.js
+++ 9_advance/promises.js
@@ -25,52 +25,85 @@
     
 // })
 
-new Promise((resolve, reject)=>{
-    setTimeout(()=>{
+// new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
        
-        resolve({name  : 'Apurv', gmail : 'apu@gmail.com'})
-    },3000)
+//         resolve({name  : 'Apurv', gmail : 'apu@gmail.com'})
+//     },3000)
     
-}).then((user)=>{
+// }).then((user)=>{
     // console.log(user);
     
-})
+// })
+
+
 
 // promiseThree.then((user)=>{
 //     console.log(user);
     
 // })
 
-let func 
-const promiseTest = new Promise((resolve, reject) => {
-    setTimeout(function(){
-        resolve({name:'bkt', gmail:'a@a'})
-    },1000)
-}).then((data)=>{
+// let func 
+// const promiseTest = new Promise((resolve, reject) => {
+//     setTimeout(function(){
+//         resolve({name:'bkt', gmail:'a@a'})
+//     },1000)
+// }).then((data)=>{
     // console.log(data);
     
-})
+// })
 
 
-const promiseFour =new Promise(function(resolve, reject){
+// const promiseFour =new Promise(function(resolve, reject){
+//     setTimeout(()=>{
+//         let err = false;
+//         if(!err){
+//             resolve({usernam : 'apurv', pass: 123})
+//         }
+//         else{reject('ERROR kuch to gadbar hai daya')}
+//     },1000)
+// })
+
+// promiseFour.then((user)=>{
+//     // console.log(user);
+//     return user.usernam
+    
+// })
+// .then((name)=>{
+//     console.log(name);
+    
+// })
+// .catch((e)=>{
+//     console.log(e);
+    
+// })
+
+
+
+
+const promiseFive = new Promise((resolve, reject)=>{
     setTimeout(()=>{
-        let err = false;
+        let err = true;
         if(!err){
-            resolve({usernam : 'apurv', pass: 123})
+            resolve({name:'apurv', pass: 1223})
+        }else{
+            reject('ERROR: js me dikkt')
         }
-        else{reject('ERROR kuch to gadbar hai daya')}
     },1000)
 })
 
 
-promiseFour.then((user)=>{
-    // console.log(user);
-    return user.usernam
+async function promiseCheck(){
     
-}).then((name)=>{
-    console.log(name);
+    try {
+        const res = await promiseFive
+        console.log(res);
+        
+    } catch (error) {
+        
+        console.log(error);
+        
+    }
     
-}).catch((e)=>{
-    console.log(e);
-    
-})
+}
+promiseCheck()
