8227277f129f595846e6c6681e42db4edf4f6e0c
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-06 19:38:22 +0530

    push

---
+++ 10_oops/oops.js
@@ -0,0 +1,12 @@
+const user = {
+    username: 'apurv',
+    loginCount : 8,
+    signedIn : true,
+    
+    getData : function(){
+        console.log(this.username);
+        
+    }
+    
+}
+user.getData()
