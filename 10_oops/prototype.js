97de3a280039076052fd170d9884df8b340d7662
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-08 19:39:49 +0530

    start

--- 10_oops/prototype.js
+++ 10_oops/prototype.js
@@ -1,7 +1,15 @@
 const name  = '   apurv       '
-console.log(name.length);
-console.log(name);
-console.log(name.trim().length);
+// console.log(name.length);
+// console.log(name);
+// console.log(name.trim().length);
 
 
 //time 30min 00 sec
+
+
+let hero = ['thor', 'spiderman']
+
+let heroPower = {
+    thor : 'hammer',
+    spiderman : 'web'
+}
