d2d1b39efdbeafd51b225d37e4f57aa546afea3d
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-09 09:24:11 +0530

    funcitions and classes

--- 10_oops/myClasses.js
+++ 10_oops/myClasses.js
@@ -1,23 +1,42 @@
 // ES 6
 
-class User {
-    constructor(username, email, password ){
+// class User {
+//     constructor(username, email, password ){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password
+//     }
+    
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUserName(){
+//         return `${this.username.toUpperCase()}`
+//     }
+    
+  
+// }
+
+// const user = new User('chai', 'chai@gamil.com', 'bkt1232')
+// console.log(user.changeUserName());
+
+// console.log(user.encryptPassword());
+
+
+
+function test(username, email, password){
         this.username = username;
         this.email = email;
         this.password = password
-    }
-    
-    encryptPassword(){
-        return `${this.password}abc`
-    }
-    changeUserName(){
-        return `${this.username.toUpperCase()}`
-    }
-    
-  
 }
 
-const user = new User('chai', 'chai@gamil.com', 'bkt1232')
-console.log(user.changeUserName());
+test.prototype.encry = function(){
+    return `${this.password}abc`
+}
+test.prototype.uppercase = function(){
+    return `${this.username.toUpperCase()}`
+}
 
-console.log(user.encryptPassword());
+const tea = new test('tea', 'tea@gamil.com', 'tea123')
+console.log(tea.uppercase());
+console.log(tea.encry());
