53f603e314f46024fec1b25691a362c633968b92
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-09 09:27:02 +0530

    done classes

--- 10_oops/myClasses.js
+++ 10_oops/myClasses.js
@@ -1,26 +1,32 @@
 // ES 6
 
-// class User {
-//     constructor(username, email, password ){
-//         this.username = username;
-//         this.email = email;
-//         this.password = password
-//     }
+class User {
+    constructor(username, email, password ){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
     
-//     encryptPassword(){
-//         return `${this.password}abc`
-//     }
-//     changeUserName(){
-//         return `${this.username.toUpperCase()}`
-//     }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUserName(){
+        return `${this.username.toUpperCase()}`
+    }
     
   
-// }
+}
 
-// const user = new User('chai', 'chai@gamil.com', 'bkt1232')
-// console.log(user.changeUserName());
+const user = new User('chai', 'chai@gamil.com', 'bkt1232')
+console.log(user.changeUserName());
 
-// console.log(user.encryptPassword());
+console.log(user.encryptPassword());
+
+
+
+
+// Behind the scene
+
 
 
 
