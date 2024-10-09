53249294b11c22918d4e77a9e0d1b619a85c862c
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-09 09:13:14 +0530

    push

--- 10_oops/myClasses.js
+++ 10_oops/myClasses.js
@@ -1,5 +1,23 @@
 // ES 6
 
 class User {
+    constructor(username, email, password ){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
     
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUserName(){
+        return `${this.username.toUpperCase()}`
+    }
+    
+  
 }
+
+const user = new User('chai', 'chai@gamil.com', 'bkt1232')
+console.log(user.changeUserName());
+
+console.log(user.encryptPassword());
