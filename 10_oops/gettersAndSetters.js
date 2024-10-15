e85bc2fde9b465a5cac5a13b2e2b16adb1e6ee2c
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2024-10-15 19:13:09 +0530

    push

--- 10_oops/gettersAndSetters.js
+++ 10_oops/gettersAndSetters.js
@@ -1,3 +1,12 @@
 class users{
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+    
+    get email(){
+        return this.email.toUpperCase()
+    }
     
 }
+
